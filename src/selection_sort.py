3f696c290930ae0f2bf69f54f445bca0d3ff225d
Kenta57 <0121kenta@gmail.com>
2021-06-16 18:34:03 +0900

    選択ソートの実装

---
+++ src/selection_sort.py
@@ -0,0 +1,17 @@
+from typing import List
+
+def selection_sort(A:List) -> List:
+    n = len(A)
+    for i in range(n):
+        min_index = i
+        for j in range(i,n):
+            if A[min_index] > A[j]:
+                min_index = j
+        A[min_index], A[i] = A[i], A[min_index]
+
+    return A
+
+if __name__ == '__main__':
+    l = [3,4,6,3,5,56,4,32,66,4,-1]
+    l_s = selection_sort(l)
+    print(l_s)
