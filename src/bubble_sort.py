afe3a99c84a207864c9bd582bb2d658af545325b
Kenta57 <0121kenta@gmail.com>
2021-06-16 18:22:21 +0900

    bubbleソートの実装

---
+++ src/bubble_sort.py
@@ -0,0 +1,17 @@
+from typing import List
+
+def bubble_sort(A : List) -> List:
+    n = len(A)
+    flag = True
+    while flag:
+        flag = False
+        for i in range(n-1,0,-1):
+            if A[i] < A[i-1]:
+                A[i], A[i-1] = A[i-1], A[i]
+                flag = True
+    return A
+
+if __name__ == '__main__':
+    l = [3,4,6,3,5,56,4,32,66,4,-1]
+    l_s = bubble_sort(l)
+    print(l_s)
